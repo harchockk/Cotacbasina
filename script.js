93925f1041a7f2e38f1808673180768560997a3a
harchockk <urao54840@gmail.com>
2026-01-29 18:47:52 +0300

    qwf

---
+++ script.js
@@ -0,0 +1,77 @@
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+
+ctx.fillRect(50,50,100,60);
+
+ctx.fillStyle = "rgb(200,0,0)";
+ctx.fillRect(130,130,55,50);
+
+ctx.fillStyle = "rgb(0,0,200,0.5)";
+ctx.fillRect(100,100,55,50);
+
+
+ctx.fillRect(200,200,100,100);
+ctx.clearRect(220,220,60,60);
+ctx.strokeRect(225,225,50,50);
+
+
+ctx.beginPath();
+ctx.moveTo(10,10);
+ctx.lineTo(200,50);
+ctx.stroke();
+
+
+
+ctx.beginPath();
+ctx.moveTo(75,50);
+ctx.lineTo(100,75);
+ctx.lineTo(100,25);
+ctx.fill();
+
+
+ctx.beginPath();
+ctx.moveTo(20,300);
+ctx.lineTo(100,300);
+ctx.lineTo(20,100);
+ctx.fill();
+
+
+ctx.beginPath();
+ctx.moveTo(100,300);
+ctx.lineTo(100,100);
+ctx.lineTo(155,100);
+
+ctx.closePath();
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(100,100,40,0,Math.PI *2);
+ctx.fillStyle = "rgb(0,200,0,0.7)";
+ctx.fill();
+
+ctx.fillStyle = "rgb(0,100,100,0.7)";
+ctx.beginPath();
+ctx.arc(75,75,50,0,Math.PI *2, true);
+ctx.moveTo(110,75);
+ctx.arc(75,75,35,0,Math.PI *2, false);
+ctx.moveTo(65,65);
+ctx.arc(60,65,5,0,Math.PI *2, true);
+ctx.moveTo(95,65);
+ctx.arc(90,65,5,0,Math.PI *2, true);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.moveTo(75,25);
+ctx.quadraticCurveTo(25,25,25,62.5);
+ctx.quadraticCurveTo(25,100,50,100);
+ctx.quadraticCurveTo(50,120,30,125);
+ctx.quadraticCurveTo(60,120,65,100);
+ctx.quadraticCurveTo(125,100,125,62.5);
+ctx.quadraticCurveTo(125,25,75,25);
+ctx.stroke();
+ctx.fill();
